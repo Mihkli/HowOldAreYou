dff5029b28dc659130c743e612c2f634f1865704
Opilane TTHK <opilane@tthk.ee>
2021-09-01 12:02:21 +0300

    first commit

---
+++ FullName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja käest eesnime
+            //programm küsib kasutaja käest perekonna nime
+            //programm tervitab kasutajat kasutades neid andmeid
+            Console.WriteLine("What is your first name");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {firstname}!");
+            Console.WriteLine($"What is your last name?");
+            string lastname = Console.ReadLine();
+            Console.WriteLine($"Pleasure to meet you {firstname} {lastname}");
+            Console.WriteLine($"How old are you {lastname}");                                
+
+
+            Console.Read();
+        }
+    }
+}
